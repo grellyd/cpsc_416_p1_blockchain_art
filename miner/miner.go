74d4017cc0bb7204061f2694b021716304b73917
Sharon Yang <l5w8@bowen.ugrad.cs.ubc.ca>
2018-01-31 16:00:54 -0800

    Moved key generation to helper function

--- miner/miner.go
+++ miner/miner.go
@@ -10,22 +10,26 @@ import (
 )
 
 func main() {
-	curve := elliptic.P256()
-	r := strings.NewReader("Hello, Reader!")
-
-	keys, err := ecdsa.GenerateKey(curve, r)
-	CheckError(err)
+	GetKeyPair() // Temporary, keys will be passed in as command line args
+	// Need to print then pass to client
 	// Connect to server
 	localIP := "127.0.0.1:0"
 	//serverAddr := "tbd"
 	var nbrs [256]int
 	m := minerlib.Miner{"server addr", nbrs, localIP}
 
-	fmt.Printf("miner ip: %v, m: %v, keys.pub %v\n", localIP, m, keys.PublicKey)
+	fmt.Printf("miner ip: %v, m: %v, \n", localIP, m)
 }
 
 func CheckError(err error) {
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 	}
+}
+
+func GetKeyPair() {
+	curve := elliptic.P256()
+	r := strings.NewReader("Hello, Reader!")
+	keys, _ := ecdsa.GenerateKey(curve, r)
+	fmt.Printf("Keys: %v\n", keys.PublicKey)
 }
