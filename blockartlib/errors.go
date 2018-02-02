0cf7b8a40f9c7cd151a201bbbaac27283b45b459
grellyd <graham.brown137@gmail.com>
2018-02-02 10:36:54 -0800

    extracted out errors

---
+++ blockartlib/errors.go
@@ -0,0 +1,80 @@
+package blockartlib
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// <ERROR DEFINITIONS>
+
+// These type definitions allow the application to explicitly check
+// for the kind of error that occurred. Each API call below lists the
+// errors that it is allowed to raise.
+//
+// Also see:
+// https://blog.golang.org/error-handling-and-go
+// https://blog.golang.org/errors-are-values
+
+// Contains address IP:port that art node cannot connect to.
+type DisconnectedError string
+
+func (e DisconnectedError) Error() string {
+	return fmt.Sprintf("BlockArt: cannot connect to [%s]", string(e))
+}
+
+// Contains amount of ink remaining.
+type InsufficientInkError uint32
+
+func (e InsufficientInkError) Error() string {
+	return fmt.Sprintf("BlockArt: Not enough ink to addShape [%d]", uint32(e))
+}
+
+// Contains the offending svg string.
+type InvalidShapeSvgStringError string
+
+func (e InvalidShapeSvgStringError) Error() string {
+	return fmt.Sprintf("BlockArt: Bad shape svg string [%s]", string(e))
+}
+
+// Contains the offending svg string.
+type ShapeSvgStringTooLongError string
+
+func (e ShapeSvgStringTooLongError) Error() string {
+	return fmt.Sprintf("BlockArt: Shape svg string too long [%s]", string(e))
+}
+
+// Contains the bad shape hash string.
+type InvalidShapeHashError string
+
+func (e InvalidShapeHashError) Error() string {
+	return fmt.Sprintf("BlockArt: Invalid shape hash [%s]", string(e))
+}
+
+// Contains the bad shape hash string.
+type ShapeOwnerError string
+
+func (e ShapeOwnerError) Error() string {
+	return fmt.Sprintf("BlockArt: Shape owned by someone else [%s]", string(e))
+}
+
+// Empty
+type OutOfBoundsError struct{}
+
+func (e OutOfBoundsError) Error() string {
+	return fmt.Sprintf("BlockArt: Shape is outside the bounds of the canvas")
+}
+
+// Contains the hash of the shape that this shape overlaps with.
+type ShapeOverlapError string
+
+func (e ShapeOverlapError) Error() string {
+	return fmt.Sprintf("BlockArt: Shape overlaps with a previously added shape [%s]", string(e))
+}
+
+// Contains the invalid block hash.
+type InvalidBlockHashError string
+
+func (e InvalidBlockHashError) Error() string {
+	return fmt.Sprintf("BlockArt: Invalid block hash [%s]", string(e))
+}
+
+// </ERROR DEFINITIONS>
+////////////////////////////////////////////////////////////////////////////////////////////
+
