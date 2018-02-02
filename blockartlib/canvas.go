1807ce98ecfa60cf16ae76352f99aa9af4ab536d
Sharon Yang <l5w8@bowen.ugrad.cs.ubc.ca>
2018-02-02 10:27:23 -0800

    Added a tree type and canvas type

---
+++ blockartlib/canvas.go
@@ -0,0 +1,13 @@
+package blockartlib
+
+// Canvas data structure that validates additions/removals
+// from the canvas
+
+type CanvasData struct {
+	Points [][]int // Need to update the dimensions when creating canvas
+	Operations map[string]Operation
+}
+
+func (cd *CanvasData) ValidateAddShape() (b bool) {
+	return b
+}
