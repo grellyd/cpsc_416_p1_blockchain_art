ee8a77356e665e06af9f59182079ca375d809ea5
grellyd <graham.brown137@gmail.com>
2018-02-12 14:18:35 -0800

    extracted out to individual funcs

--- keys/keys.go
+++ keys/keys.go
@@ -18,22 +18,37 @@ func Generate() (privateKey *ecdsa.PrivateKey, publicKey *ecdsa.PublicKey, err e
 }
 
 func Encode(privateKey *ecdsa.PrivateKey, publicKey *ecdsa.PublicKey) (string, string) {
+	return EncodePrivateKey(privateKey), EncodePublicKey(publicKey)
+}
+
+func EncodePrivateKey(privateKey *ecdsa.PrivateKey) (string) {
 	x509Encoded, _ := x509.MarshalECPrivateKey(privateKey)
+	var b []byte = x509Encoded
+	return string(b)
+}
+
+func EncodePublicKey(publicKey *ecdsa.PublicKey) (string) {
 	x509EncodedPub, _ := x509.MarshalPKIXPublicKey(publicKey)
 
-	var b []byte = x509Encoded
 	var c []byte = x509EncodedPub
-	return string(b), string(c)
-
+	return string(c)
 }
 
 func Decode(pemEncoded string, pemEncodedPub string) (*ecdsa.PrivateKey, *ecdsa.PublicKey) {
+	return DecodePrivateKey(pemEncoded), DecodePublicKey(pemEncodedPub)
+}
+
+func DecodePrivateKey(pemEncoded string) (*ecdsa.PrivateKey) {
 	privateKey, _ := x509.ParseECPrivateKey([]byte(pemEncoded))
+	return privateKey
+}
+
+func DecodePublicKey(pemEncodedPub string) (*ecdsa.PublicKey) {
 	genericPublicKey, _ := x509.ParsePKIXPublicKey([]byte(pemEncodedPub))
 	publicKey := genericPublicKey.(*ecdsa.PublicKey)
-
-	return privateKey, publicKey
+	return publicKey
 }
+
 
 func MatchPublicKeys(key0 *ecdsa.PublicKey, key1 *ecdsa.PublicKey) (match bool) {
 	return reflect.DeepEqual(key0, key1)
